0fb5310ba67333fc02ae53242546cd71847cc2fe
lewiih001 <lewismunyambu@gmail.com>
2022-11-20 19:49:04 +0300

    understood get and post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+   Game.all .to_json
   end
-
+post '/games/new' do
+  Game.create
+end
+delete "/games/:id" do
+  deleted = Game.find(params[:id])
+  deleted.destroy
+  deleted.to_json
+end
+patch "/games/:id" do 
+  updated = Game.find(params[:id])
+  updated.update(
+    title: params[:title],
+    genre: params[:genre],
+    price: params[:price],
+    platform: params[:platform]
+    
+  )
+  updated.to_json
+end
 end
